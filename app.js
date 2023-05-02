ffd50b2734d5b64e737bda2737c3b31f2b75ad09
Conrad <conrad@estaciona.com>
2023-05-02 13:32:35 -0300

    create Exchange instance

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict'
+const Exchange = require('./exchange')
 const Order = require('./order')
-const OrderBook = require('./orderbook')
 const Trade = require('./trade')
-const orderbook = new OrderBook('BTC/USD')
+
+const pairs = ['BTC/USD', 'ETH/USD', 'ETH/BTC']
+const bfx = new Exchange(pairs)
 
 const order = new Order('buy', 1, 'BTC/USD', 30000)
 const sellOrder1 = new Order('sell', 2, 'BTC/USD', 30000)
 const sellOrder2 = new Order('sell', 2, 'BTC/USD', 29900)
-orderbook.addOrder(sellOrder1)
-orderbook.addOrder(sellOrder2)
-const matches = orderbook.match(order)
+
+bfx.books[order.pair].addOrder(sellOrder1)
+bfx.books[order.pair].addOrder(sellOrder2)
+const matches = bfx.books[order.pair].match(order)
 
 console.log('Looking for matches for:')
 console.log(order)
@@ -19,7 +22,7 @@ if (matches.length > 0) {
     const trade = new Trade(order, matches)
     console.log('Trade prepared')
     console.log(trade)
-    orderbook.execute(trade)
+    bfx.books[order.pair].execute(trade)
     console.log('Trade executed')
     console.log(trade)
   } else {
