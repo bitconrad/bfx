26e3a3ee7644fcde7dd66405daa52c3a3f406e54
Conrad <conrad@estaciona.com>
2023-05-02 16:29:04 -0300

    add peerLookup

--- client.js
+++ client.js
@@ -4,10 +4,12 @@ const debug = require('debug')('app')
 debug.enabled = process.argv.includes('-d')
 
 class Client {
+  #maxRetry
+
   constructor(link) {
     this.link = link
     this.rpc = this.initRPC(link)
-    this.maxRetry = 10 // TODO: make this dynamic based on peer count
+    this.#maxRetry = 0
   }
 
   initRPC(link) {
@@ -16,17 +18,39 @@ class Client {
     return rpc
   }
 
-  sendOrderRequest(order, retries = 0) {
-    if (retries < this.maxRetry) {
+  peerLookup() {
+    return new Promise((resolve, reject) => {
+      this.link.lookup('order', (err, peers) => {
+        if (err) {
+          reject(err)
+        } else {
+          this.#maxRetry = peers && peers.length ? peers.length * 2 : 0
+          // TODO: optimize maxRetry to make sure we reach all peers before giving up
+          // HotFix: #maxRetry = peers.length * 2
+          resolve()
+        }
+      })
+    })
+  }
+
+  async sendOrderRequest(order, retries = 0) {
+    try {
+      await this.peerLookup()
+    } catch (err) {
+      debug(`ERROR on peer lookup: ${err}`)
+      return
+    }
+    if (retries < this.#maxRetry) {
       this.rpc.request('order', {order: order}, {timeout: 5000}, (err, res) => {
         retries++
         if (err) {
-          // Network error
+          // Network error try again
           this.sendOrderRequest(order, retries)
           debug(`ERROR on request: ${err}`)
         } else {
           // TODO: look for a match and return it
           debug(res)
+          // TODO: if no match was found retry with another peer
         }
       })
     } else {
