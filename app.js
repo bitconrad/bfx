fee97614f8eae7775be4aa8d7ad8ce2ba0c06dc2
Conrad <conrad@estaciona.com>
2023-05-02 12:51:56 -0300

    update match test

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const OrderBook = require('./orderbook')
 const orderbook = new OrderBook('BTC/USD')
 
 const buyOrder = new Order('buy', 1, 'BTC/USD', 30000)
-const sellOrder = new Order('sell', 2, 'BTC/USD', 30000)
-orderbook.addOrder(sellOrder)
+const sellOrder1 = new Order('sell', 2, 'BTC/USD', 30000)
+const sellOrder2 = new Order('sell', 2, 'BTC/USD', 29900)
+orderbook.addOrder(sellOrder1)
+orderbook.addOrder(sellOrder2)
 const matches = orderbook.match(buyOrder)
 
 console.log('Looking for matches for:')
