c618d970418b4010c17c01bcd2063058451aa5ea
Conrad <conrad@estaciona.com>
2023-05-02 12:35:58 -0300

    implement OrderBook

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 'use strict'
 const Order = require('./order')
-const order = new Order('buy', 1, 'BTC/USD', 1000)
-console.log(order)
+const OrderBook = require('./orderbook')
+const orderbook = new OrderBook('BTC/USD')
+
+const buyOrder = new Order('buy', 1, 'BTC/USD', 30000)
+const sellOrder = new Order('sell', 1, 'BTC/USD', 31000)
+orderbook.addOrder(buyOrder)
+orderbook.addOrder(sellOrder)
+console.log(orderbook)
