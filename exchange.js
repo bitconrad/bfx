7a1c42a2176af02b704e6c4ae0e615a01255ef62
Conrad <conrad@estaciona.com>
2023-05-02 13:45:11 -0300

    use debug instead of console.log

--- exchange.js
+++ exchange.js
@@ -1,4 +1,7 @@
 'use strict'
+const debug = require('debug')('app')
+debug.enabled = process.argv.includes('-d')
+
 const OrderBook = require('./orderbook')
 const Trade = require('./trade')
 
@@ -17,25 +20,25 @@ class Exchange {
   }
 
   newOrder(order) {
-    console.log('New order created')
-    console.log(order)
+    debug('New order created')
+    debug(order)
     if (!order.pair || !this.books[order.pair]) {
-      console.log(`Book not found for pair ${order.pair}`)
+      debug(`Book not found for pair ${order.pair}`)
     } else {
       this.books[order.pair].addOrder(order)
       const matches = this.books[order.pair].match(order)
       if (matches.length > 0) {
         if (order.lock()) {
-          console.log(`Found matching orders for ${order.id}`)
-          console.log('Lock acquired, performing trade')
+          debug(`Found matching orders for ${order.id}`)
+          debug('Lock acquired, performing trade')
           const trade = new Trade(order, matches)
-          console.log('Trade prepared')
-          console.log(trade)
+          debug('Trade prepared')
+          debug(trade)
           this.books[order.pair].execute(trade)
-          console.log('Trade executed')
-          console.log(trade)
+          debug('Trade executed')
+          debug(trade)
         } else {
-          console.log(
+          debug(
             `Order ${order.id} is already locked, cancelling to avoid a race condition`
           )
         }
