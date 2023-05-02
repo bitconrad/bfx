6d02ef609dfd91c35b5180f5c7445d7cae9576eb
Conrad <conrad@estaciona.com>
2023-05-02 12:20:39 -0300

    assign a uuid and creation date to Order

--- order.js
+++ order.js
@@ -1,11 +1,14 @@
 'use strict'
+const {v4: uuidv4} = require('uuid')
 
 class Order {
   constructor(side, quantity, pair, price) {
+    this.id = uuidv4()
     this.side = side
     this.quantity = quantity
     this.pair = pair
     this.price = price
+    this.created = Date.now()
   }
 }
 
