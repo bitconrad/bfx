d59c8752f5e11532d5863b5d8f2382e924bfa0a2
Conrad <conrad@estaciona.com>
2023-05-02 13:21:06 -0300

    create a method to perform trades in OrderBook

--- orderbook.js
+++ orderbook.js
@@ -55,6 +55,43 @@ class OrderBook {
     }
     return result
   }
+
+  execute(trade) {
+    if (
+      trade.order &&
+      trade.order.quantity > 0 &&
+      trade.matches &&
+      trade.matches.length > 0
+    ) {
+      for (let i = 0; i < trade.matches.length; i++) {
+        // Check if the matched order is locked before executing
+        if (!trade.matches[i].isLocked()) {
+          continue
+        }
+
+        // If remaining quantity is greater than the matched order, execute the full matched order
+        let executed = 0
+        if (trade.remaining > trade.matches[i].quantity) {
+          executed = trade.matches[i].quantity
+        } else {
+          executed = trade.remaining
+        }
+
+        // Update the remaining quantity of the orders and the trade
+        trade.order.quantity -= executed
+        trade.matches[i].quantity -= executed
+        trade.remaining -= executed
+        trade.executed += executed
+
+        // Unlock the matched order and remove it from the order book if fully executed
+        trade.matches[i].unlock()
+        if (trade.remaining == 0) {
+          break
+        }
+      }
+      trade.updateStatus()
+    }
+  }
 }
 
 module.exports = OrderBook
