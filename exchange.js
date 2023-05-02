2cc4a0ef0664ae2ed47cb1208c7cb830ff840051
Conrad <conrad@estaciona.com>
2023-05-02 13:51:16 -0300

    assign a uuid to the Exchange instance

--- exchange.js
+++ exchange.js
@@ -2,11 +2,16 @@
 const debug = require('debug')('app')
 debug.enabled = process.argv.includes('-d')
 
+const {v4: uuidv4} = require('uuid')
+
 const OrderBook = require('./orderbook')
 const Trade = require('./trade')
 
 class Exchange {
   constructor(pairs) {
+    this.peerId = uuidv4()
+    debug(`Peer id: ${this.peerId}`)
+
     this.books = {}
     this.initBooks(pairs)
   }
