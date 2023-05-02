a4f96d664e77631fe3aa70d7131d2aa00e057e6a
Conrad <conrad@estaciona.com>
2023-05-02 13:08:22 -0300

    lock order before performing a new trade

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 'use strict'
 const Order = require('./order')
 const OrderBook = require('./orderbook')
+const Trade = require('./trade')
 const orderbook = new OrderBook('BTC/USD')
 
-const buyOrder = new Order('buy', 1, 'BTC/USD', 30000)
+const order = new Order('buy', 1, 'BTC/USD', 30000)
 const sellOrder1 = new Order('sell', 2, 'BTC/USD', 30000)
 const sellOrder2 = new Order('sell', 2, 'BTC/USD', 29900)
 orderbook.addOrder(sellOrder1)
 orderbook.addOrder(sellOrder2)
-const matches = orderbook.match(buyOrder)
+const matches = orderbook.match(order)
 
 console.log('Looking for matches for:')
-console.log(buyOrder)
+console.log(order)
 if (matches.length > 0) {
-  console.log('Found matches:')
-  console.log(matches)
+  if (order.lock()) {
+    console.log('Lock acquired, performing trade')
+    const trade = new Trade(order, matches)
+    console.log(trade)
+  } else {
+    console.log('Order is locked, cannot trade')
+  }
 } else {
   console.log('No matches found')
 }
