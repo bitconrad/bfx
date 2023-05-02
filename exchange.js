a37e1d779bfe1dc244378ff9df8b19b8d0cb03c7
Conrad <conrad@estaciona.com>
2023-05-02 13:41:39 -0300

    add newOrder method to Exchange class

--- exchange.js
+++ exchange.js
@@ -1,5 +1,6 @@
 'use strict'
 const OrderBook = require('./orderbook')
+const Trade = require('./trade')
 
 class Exchange {
   constructor(pairs) {
@@ -12,6 +13,33 @@ class Exchange {
       pairs.forEach((pair) => {
         this.books[pair] = new OrderBook(pair)
       })
+    }
+  }
+
+  newOrder(order) {
+    console.log('New order created')
+    console.log(order)
+    if (!order.pair || !this.books[order.pair]) {
+      console.log(`Book not found for pair ${order.pair}`)
+    } else {
+      this.books[order.pair].addOrder(order)
+      const matches = this.books[order.pair].match(order)
+      if (matches.length > 0) {
+        if (order.lock()) {
+          console.log(`Found matching orders for ${order.id}`)
+          console.log('Lock acquired, performing trade')
+          const trade = new Trade(order, matches)
+          console.log('Trade prepared')
+          console.log(trade)
+          this.books[order.pair].execute(trade)
+          console.log('Trade executed')
+          console.log(trade)
+        } else {
+          console.log(
+            `Order ${order.id} is already locked, cancelling to avoid a race condition`
+          )
+        }
+      }
     }
   }
 }
